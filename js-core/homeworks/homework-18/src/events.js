6d788d79b42bde58eb35ec37d985c1d6a61ba37c
Sergey <sergey.diod@gmail.com>
2018-08-02 16:29:03 +0300

    homework 18

--- js-core/homeworks/homework-18/src/events.js
+++ js-core/homeworks/homework-18/src/events.js
@@ -1,30 +1,35 @@
 
-let block1 = document.querySelector(".one").children[0]
-let block2 = document.querySelector(".two").children[0]
-let block3 = document.querySelector(".three").children[0]
+let block1 = document.querySelector(".one")
+let block2 = document.querySelector(".two")
+let block3 = document.querySelector(".three")
 
 
 let startPosition = 0;
 let mousePositionX = 0;
 
-block1.addEventListener("mousedown", function(event){
+
+block1.parentElement.addEventListener("mousedown", function(event){
+    
     if(event.target.className == "resize"){
-        let activeBlock = block1.children
-        console.log(block1.parentElement)
         startPosition = event.target.parentElement.offsetLeft
-        console.log(event.clientX)
-        block1.addEventListener("mousemove",setMaxWidth)
+        block1.parentElement.addEventListener("mousemove", function(){
+            setMaxWidth(block1)
+        })
     }
 })
-block1.addEventListener("mouseup", function(event){
-    block1.removeEventListener("mousemove",setMaxWidth)
+block1.parentElement.addEventListener("mouseup", function(event){
+    console.log(event)
+    console.log(this)
+    this.removeEventListener("mousemove",function(){
+        setMaxWidth(block1)
+    })
 })
   
-function setMaxWidth(){
+function setMaxWidth(elem){
+    let blockStartWidth = block1.offsetWidth
     mousePositionY = event.clientX
-    let width = mousePositionY - startPosition + 10
-    console.log(event.target)
-    event.target.style.maxWidth = width+"px"
+    let width = (mousePositionY + startPosition) - blockStartWidth
+    elem.style.flexBasis = width+"px"
 }
 
 
